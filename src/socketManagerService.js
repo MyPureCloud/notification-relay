00d3b8711962ef31fb1e1f141b40b0f27b2aeff2
Tim Smith <tim.j.smith@genesys.com>
2017-08-15 12:02:48 -0600

    API-2731 updating socket manager

--- src/socketManagerService.js
+++ src/socketManagerService.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+const net = require('net');
 const Q = require('q');
 const WebSocket = require('ws');
 
@@ -7,8 +8,10 @@ const config = require('./configService');
 
 
 
-var sockets = {};
+var webSockets = {};
 var webSocketServers = {};
+var tcpSockets = {};
+var tcpSocketServers = {};
 
 
 
@@ -16,7 +19,10 @@ function SocketManager() {
 	this.log = new Logger('SocketManager', config.data.get('settings.logLevel'));
 }
 
-// Web Sockets
+
+/* 
+ * Web Sockets
+ */
 
 SocketManager.prototype.connectWebSocket = function(url, callback) {
 	var _this = this;
@@ -25,34 +31,34 @@ SocketManager.prototype.connectWebSocket = function(url, callback) {
 		var socket = new WebSocket(url);
 		// Connection opened
 		socket.on('open', function (event) {
-		    _this.log.silly(`WebSocket opened to ${url}`);
-		    callback('open', event);
+	    _this.log.silly(`WebSocket opened to ${url}`);
+	    callback('open', event);
 		});
 
 		// Listen for messages
 		socket.on('message', function (event) {
-		    _this.log.silly(`Message from server ${event}`);
-		    callback('message', JSON.parse(event));
+	    _this.log.silly(`Message from server ${event}`);
+	    callback('message', JSON.parse(event));
 		});
 
-		sockets[url] = socket;
+		webSockets[url] = socket;
 	} catch(err) {
 		_this.log.error(err);
 	}
 };
 
 SocketManager.prototype.getWebSocket = function(url) {
-	return sockets[url];
+	return webSockets[url];
 };
 
-SocketManager.prototype.listenWebSocket = function(port, connectionCallback, errorCallback, listeningCallback, headersCallback) {
+SocketManager.prototype.listenWebSocket = function(port, connectionCallback, errorCallback, listeningCallback, closedCallback) {
 	// https://github.com/websockets/ws/blob/master/doc/ws.md#new-websocketserveroptions-callback
 	var wss = new WebSocket.Server({ port: port });
 	
 	wss.on('connection', connectionCallback);
 	wss.on('error', errorCallback);
-	wss.on('headers', headersCallback);
 	wss.on('listening', listeningCallback);
+	wss.on('close', closedCallback);
 
 	webSocketServers[port] = wss;
 
@@ -65,22 +71,54 @@ SocketManager.prototype.getWebSocketServer = function(port) {
 	return webSocketServers[port];
 };
 
-// TCP sockets
+
+/* 
+ * TCP Sockets
+ */
 
 SocketManager.prototype.connectTcpSocket = function(url, callback) {
+	var _this = this;
 
+	try {
+		var socket = new net.Socket();
+
+		socket.connect(url, function() {
+	    _this.log.silly(`TCP socket opened to ${url}`);
+	    callback('open', event);
+		});
+
+		socket.on('data', function(data) {
+	    _this.log.silly(`Message from server ${event}`);
+	    callback('message', JSON.parse(event));
+		});
+
+		tcpSockets[url] = socket;
+	} catch(err) {
+		_this.log.error(err);
+	}
 };
 
-SocketManager.prototype.getTcpSocket = function(url, callback) {
-
+SocketManager.prototype.getTcpSocket = function(url) {
+	return tcpSockets[url];
 };
 
-SocketManager.prototype.listenTcpSocket = function(url, callback) {
+SocketManager.prototype.listenTcpSocket = function(port, connectionCallback, errorCallback, listeningCallback, closedCallback) {
+	var server = net.createServer();
 
+	server.on('connection', connectionCallback);
+	server.on('error', errorCallback);
+	server.on('listening', listeningCallback);
+	server.on('close', closedCallback);
+
+	this.log.info(`Listening for incoming TCP socket connections on port ${port}`);
+
+	server.listen(port);
+
+	tcpSocketServers[port] = server;
 };
 
-SocketManager.prototype.getTcpSocketServer = function(url, callback) {
-
+SocketManager.prototype.getTcpSocketServer = function(port) {
+	return tcpSocketServers[port];
 };
 
 
